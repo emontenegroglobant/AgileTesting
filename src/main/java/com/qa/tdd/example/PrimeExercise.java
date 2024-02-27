5013d7f5cad91bdbc79ef62a03655d97f91528ae
emontenegro <erika.montenegro27@gmail.com>
2024-02-27 09:28:29 -0500

    Exercise Is prime - Project

---
+++ src/main/java/com/qa/tdd/example/PrimeExercise.java
@@ -0,0 +1,27 @@
+package com.qa.tdd.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeExercise {
+
+    public boolean isPrime(int number) {
+
+        if (number < 0) {
+            System.out.println("The number: " + number + " is not prime, it's a negative number");
+            return false;
+        } else {
+            for (int i = 2; i < number; i++) {
+
+                if (number % i == 0) {
+                    System.out.println("The number: " + number + " is not prime");
+                    return false;
+                }
+            }
+            System.out.println("The number: " + number + " is prime");
+            return true;
+
+        }
+    }
+}
---
+++ src/test/java/com/qa/tdd/example/PrimeTest.java
@@ -0,0 +1,56 @@
+package com.qa.tdd.example;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void verify_number_one() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(1);
+        assertTrue(isPrime);
+    }
+
+    @Test
+    public void verify_number_four() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(4);
+        assertFalse(isPrime);
+    }
+
+    @Test
+    public void verify_number_seven() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(7);
+        assertTrue(isPrime);
+    }
+
+    @Test
+    public void verify_number_twenty() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(20);
+        assertFalse(isPrime);
+    }
+
+    @Test
+    public void verify_number_thirtyOne() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(31);
+        assertTrue(isPrime);
+    }
+
+    @Test
+    public void verify_number_negative() {
+        PrimeExercise primeExercise = new PrimeExercise();
+        boolean isPrime = primeExercise.isPrime(-3);
+        assertFalse(isPrime);
+    }
+}
